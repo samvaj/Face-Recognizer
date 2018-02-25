6fb06b9b5fc54de4e42d173e04db9f7846473cdd
Rahul <rrangith@edu.uwaterloo.ca>
2018-02-24 21:13:35 -0500

    added notifications

--- src/App.js
+++ src/App.js
@@ -23,26 +23,6 @@ const particleOptions = {
   }            
 }
 
-createNotification = (type) => {
-  return () => {
-    switch (type) {
-      case 'info':
-        NotificationManager.info('Info message');
-        break;
-      case 'success':
-        NotificationManager.success('Success message', 'Title here');
-        break;
-      case 'warning':
-        NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
-        break;
-      case 'error':
-        NotificationManager.error('Error message', 'Click me!', 5000, () => {
-          alert('callback');
-        });
-        break;
-    }
-  };
-}
 
 const initialState = {
   
@@ -77,6 +57,29 @@ class App extends Component {
       }
     })
   }
+
+  createNotification = (type) => {
+    return () => {
+      switch (type) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'success':
+          NotificationManager.success('Success message', 'Title here');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+          break;
+        case 'error':
+          NotificationManager.error('Error message', 'Click me!', 5000, () => {
+            alert('callback');
+          });
+          break;
+      }
+    };
+  }
+  
+
   calculateFaceLocation = (data) =>{
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
