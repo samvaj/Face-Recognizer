df8bc2454b4043bd302878628111d6bbe6fcf118
Rahul <rrangith@edu.uwaterloo.ca>
2018-03-06 00:26:58 -0500

    fixed prompt

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 	return (
 		<div>
-			<p className = 'f3'>{'This will detect faces in your pictures'}</p>
+			<p className = 'f3'>{'This will detect a single face in your pictures'}</p>
 			<div className = 'center'>
 				<div className = 'form center pa4 br3 shadow-5'>
 					<input className = 'f4 pa2 w-70 center' type = 'text' onChange = {onInputChange}/>
